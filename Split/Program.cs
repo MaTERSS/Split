167a718cce358b33953b8d6d3405ed326bd7e184
Ardor <Ardor@DESKTOP-BQF7240>
2025-02-11 16:24:23 +0800

    Добавьте файлы проекта.

---
+++ Split/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+ Дана строка с текстом, используя метод строки String.Split() получить массив слов, которые разделены пробелом в тексте и вывести массив, каждое слово с новой строки.
+ */
+using System;
+using System.Text;
+
+namespace CSharplight
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            string text = "Сталкер — это персонаж, исследующий опасные и загадочные зоны," + 
+                " наполненные аномалиями и артефактами, в поисках приключений и тайн, часто вдохновленный произведениями литературы и кино.";
+            Console.WriteLine($"Исходный текст:\n" + text);
+            string[] words = text.Split(' ');
+
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
